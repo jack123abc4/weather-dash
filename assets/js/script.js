f87a77d34a79d1819d2fd357cf1aeaa9225281d7
jack123abc4 <jack.r.motherway@gmail.com>
2022-08-29 22:57:33 -0400

    forecast cards display dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,17 +54,49 @@ async function getCurrentWeather(lat,lon) {
         console.log(data.weather);
         console.log(data.weather[0]);
         console.log(data.weather[0].icon);
-        console.log(document.querySelector("#weather-icon"));
-        document.querySelector("#weather-icon").setAttribute("src", "http://openweathermap.org/img/wn/"+ data.weather[0].icon + "@2x.png")
+        console.log(weatherToday.querySelector("#weather-icon"));
+        weatherToday.querySelector("#weather-icon").setAttribute("src", "http://openweathermap.org/img/wn/"+ data.weather[0].icon + "@2x.png")
 
         weatherTodayList = weatherToday.querySelector("ul");
         $(weatherTodayList).children()[0].textContent = "Temp: " + temp + "°F";
         $(weatherTodayList).children()[1].textContent = "Wind: " + wind + "MPH";
         $(weatherTodayList).children()[2].textContent = "Humidity: " + humidity + "%";
-        
-
+        getForecastedWeather(lat,lon);
         return data;
     })
+    .catch(function(error) {
+        console.log(error);
+    });
+}
+
+async function getForecastedWeather(lat,lon) {
+    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon="+ lon + "&units=imperial&appid=" + apiKey;
+    console.log(weatherURL);
+    fetch(weatherURL, {
+        method: "GET",
+    })
+    .then(function (response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var forecastIndex = 0;
+        var forecastSection = document.querySelector("#five-day-forecast");
+        for (var i = 0; i < data.list.length; i++) {
+            var forecastDate = data.list[i].dt_txt.split(" ")[0];
+            var forecastTime = data.list[i].dt_txt.split(" ")[1];
+            console.log(forecastDate,forecastTime);
+            if (forecastTime === "12:00:00") {
+                $(forecastSection).children()[forecastIndex].querySelector("h3").textContent = forecastDate;
+                forecastIndex++;
+            }
+        }
+        return data;
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
 }
 
 
